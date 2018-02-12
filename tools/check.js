28d4adae92d426aa8ba413100a6dd6a7208147c3
Victor Charpenay <victor.charpenay@siemens.com>
2018-02-12 10:03:30 +0100

    found missing type statements in interaction-patterns.jsonld.

--- tools/check.js
+++ tools/check.js
@@ -3,17 +3,42 @@ const sep = require('path').sep;
 
 const jsonld = require('jsonld').promises;
 
+const ctx = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+    'schema': 'http://schema.org/',
+    'iot': 'http://iotschema.org/'
+};
+
 function isInNamespace(node) {
-    return node['@id'].startsWith('http://iotschema.org/');
+    return (node['@id'] != undefined && node['@id'].startsWith('http://iotschema.org/')) ||
+           node.startsWith('http://iotschema.org/');
+}
+
+function isSchemaType(node) {
+    return node['@type'] != undefined && node['@type'].indexOf('http://www.w3.org/2000/01/rdf-schema#Class') > -1;
+}
+
+function isSchemaProperty(node) {
+    return node['@type'] != undefined && node['@type'].indexOf('http://www.w3.org/1999/02/22-rdf-syntax-ns#Property') > -1;
+}
+
+function isSchemaEnumeratedValue(node) {
+    // TODO not accurate
+    return node['@type'] != undefined && node['@type'].some(t => isInNamespace(t));
 }
 
 function isSchemaEntity(node) {
-    return [
-        'http://www.w3.org/1999/02/22-rdf-syntax-ns#Property',
-        'http://www.w3.org/2000/01/rdf-schema#Class'
-    ].indexOf(node['@type']) > -1 &&
-    node['http://www.w3.org/2000/01/rdf-schema#label'] &&
-    node['http://www.w3.org/2000/01/rdf-schema#comment']
+    return (isSchemaType(node) ||
+            isSchemaProperty(node) ||
+            isSchemaEnumeratedValue(node)) &&
+           node['http://www.w3.org/2000/01/rdf-schema#label'] != undefined &&
+           node['http://www.w3.org/2000/01/rdf-schema#comment'] != undefined
+}
+
+function check(node, filename) {
+    if (!isInNamespace(node)) console.log(node['@id'] + ' not in iotschema.org namespace (' + filename + ')');
+    if (!isSchemaEntity(node)) console.log(node['@id'] + ' missing @type, label or comment (' + filename + ')');
 }
 
 fs.readdirSync('..')
@@ -24,9 +49,6 @@ fs.readdirSync('..')
 
         jsonld.flatten(def)
             .then(g => {
-                g.forEach(n => {
-                    if (!isInNamespace(n)) console.log(n['@id'] + ' not in iotschema.org namespace (' + name + ')');
-                    if (!isSchemaEntity(n)) console.log(n['@id'] + ' missing @type, label or comment (' + name + ')');
-                });
+                g.forEach(n => check(n, name));
             });
     });
